ea301eb3943e7af59e412a48c86c845be4fc84b7
13754 <1375451299@qq.com>
2021-05-11 15:15:16 +0800

    05.11

--- src/Server.java
+++ src/Server.java
@@ -88,6 +88,7 @@ public class Server {
         {
             try
             {
+
                 if(!s.isClosed()){
                     System.out.println(s + "用户已连接服务器！下一步将判断是否能登录成功..");
                     //socketList.add(s);
